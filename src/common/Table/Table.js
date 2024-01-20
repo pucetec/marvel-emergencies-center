fa44f7b10805c706ba8e64119635dca732a60daf
GhulyIu <riberaguli@gmail.com>
2024-01-20 10:59:59 -0500

    [no-story][Giuliana Rivera] Atoms created

---
+++ src/common/Table/Table.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Table = () => {
+  return (
+    <div>
+      <table>
+        <tr>
+          <th>#</th>
+          <th>Emergencia</th>
+          <th>Acciones</th>
+        </tr>
+      </table>
+    </div>
+  );
+};
+
+export default Table;
