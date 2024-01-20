5c4fd3aa22ea1dd92723644b114fbd9e3a653498
Pablo <pablomjordan17@gmail.com>
2024-01-20 10:56:41 -0500

    feat:[no-story][Pablo Jordan] first design done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <h1>Central de Emergencias</h1>
+      </div>
+      <div>
+        <label>Emergencia</label>
+        <input placeholder="emergencia"></input>
+      </div>
+      <div>
+        <button>Ingresar</button>
+      </div>
+      <div>
+        <h2>Emergencias sin asignar</h2>
+      </div>
+      <div>
+        <table>
+          <tr>
+            <th>#</th>
+            <th>Emergencia</th>
+            <th>Acciones</th>
+          </tr>
+        </table>
+      </div>
+      <div>
+        <h2>Emergencias Asignadas</h2>
+      </div>
+      <div>
+        <table>
+          <tr>
+            <th>#</th>
+            <th>Emergencia</th>
+            <th>Heroe</th>
+            <th>Acciones</th>
+          </tr>
+        </table>
+      </div>
     </div>
   );
 }
