2023b75a64e8c85450cb7bb9375db99c2594299d
neithanmervil <149252798+neithanmervil@users.noreply.github.com>
2024-01-20 11:09:41 -0500

    feat:[Neithan-Mervil][Neithan] marvel emergencies center

--- src/App.js
+++ src/App.js
@@ -4,20 +4,23 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <div>
+        <br></br>
+        <center>Central de emergencias</center>
+      </div>
+      <div>
+      <br></br>
+        <label for="emerg">Emergencias</label>
+        <input type='text' id='emerg'/>
+        <button>Ingresar</button>
+
+      </div>
+      <br></br>
+      <div>
+        <center title=''>Emergencia sin asignar</center>
+      </div>
+        
     </div>
   );
 }
