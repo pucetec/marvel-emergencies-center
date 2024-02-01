b0deb74eeaf857b0bb0409df4284d12f242b38aa
Isaac Mullo <isaacmulloaimg@gmail.com>
2024-02-01 15:04:20 -0500

    feat: [nostory] [Isaac Mullo] Assign hero and Reassing hero

--- src/EmergencyList.js
+++ src/EmergencyList.js
@@ -1,16 +1,23 @@
+// EmergencyList.js
 import React, { useContext } from 'react';
 import { EmergencyContext } from './EmergencyContext';
 
 const EmergencyList = () => {
-  const { emergencies, assignHeroToEmergency, removeEmergency, openModal } = useContext(EmergencyContext);
+  const { emergencies, assignHeroToEmergency, removeEmergency, openModal, heroes } = useContext(EmergencyContext);
+
+  // Encuentra el nombre del héroe basado en el heroId
+  const getHeroName = (heroId) => {
+    const hero = heroes.find(h => h.id === heroId);
+    return hero ? hero.name : 'Sin héroe asignado';
+  };
 
   return (
     <>
       <h2>Emergencias sin asignar</h2>
       <div>
-        {emergencies.filter(e => !e.heroId).map((emergency) => (
+        {emergencies.filter(e => !e.heroId).map((emergency, index) => (
           <div key={emergency.id}>
-            <span>{emergency.description}</span>
+            <span>{index + 1}. {emergency.description}</span>
             <button onClick={() => openModal(emergency.id)}>Asignar Héroe</button>
             <button onClick={() => removeEmergency(emergency.id)}>Eliminar</button>
           </div>
@@ -18,10 +25,11 @@ const EmergencyList = () => {
       </div>
       <h2>Emergencias Asignadas</h2>
       <div>
-        {emergencies.filter(e => e.heroId).map((emergency) => (
+        {emergencies.filter(e => e.heroId).map((emergency, index) => (
           <div key={emergency.id}>
-            <span>{emergency.description}</span>
+            <span>{index + 1}. {emergency.description} - Héroe Asignado: {getHeroName(emergency.heroId)}</span>
             <button onClick={() => openModal(emergency.id)}>Reasignar Héroe</button>
+            <button onClick={() => removeEmergency(emergency.id)}>Eliminar</button>
           </div>
         ))}
       </div>
