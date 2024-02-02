81cb3d1955a3ddf0914472a77b6b678e767d0b31
neithanmervil <149252798+neithanmervil@users.noreply.github.com>
2024-02-02 00:24:42 -0500

    Fix:[Neithan-Mervil-2][Neithan] add TableEmergency

--- src/common/TableEmergency/TableEmergency.js
+++ src/common/TableEmergency/TableEmergency.js
@@ -2,26 +2,20 @@ import React from 'react'
 import { useState } from 'react'
 
 export const TableEmergency = () => {
-  function AddTable() {
-    const [table, setTable] = useState("");
-    return({
-
-    })
-    
-  <div align = 'center'>
-    <table className='TableEmergency'>
-    <tr>
-      <th>#</th>
-      <th>Emergencia</th>
-      <th>Acciones</th>
-    </tr>
-    <tr>
-      <td></td>
-      <td></td>
-      <td></td>
-    </tr>
-  </table>
-  </div>
-  )
-}
+  const [row, setRow] = useState("");
+  return(
+    <table>
+      <tr>
+        <th>#</th>
+        <th>Emergencia</th>
+        <th>Acciones</th>
+      </tr>
+      <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+    </table>
+  );
+};
 
