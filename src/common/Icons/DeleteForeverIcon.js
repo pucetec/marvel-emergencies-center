dbda106cb0f2cc021cc7fdc576327caa042a602e
Rafael Loor <rafaelloor@Rafaels-MacBook-Pro.local>
2024-02-01 22:12:32 -0500

    feat:[no-story][Rafael-loor]emergency methods implements

---
+++ src/common/Icons/DeleteForeverIcon.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { DeleteForeverIcon as CustomDeleteIcon } from "@mui/icons-material";
+import IconButton from "@mui/material";
+
+const DeleteForeverIcon = ({}) => {
+  return (
+    <IconButton>
+      <CustomDeleteIcon />
+    </IconButton>
+  );
+};
+
+export default DeleteForeverIcon;
